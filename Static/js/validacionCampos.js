5c81eff27248c2d1f995e92cef4681e5bed5d91c
Azoka23 <marcela68.ma@gmail.com>
2024-04-17 13:50:44 -0300

    tipo letra,register,validacion

---
+++ Static/js/validacionCampos.js
@@ -0,0 +1,21 @@
+document.getElementById('register-form').addEventListener('submit', function(event) {
+    var form = event.target;
+    var inputs = form.querySelectorAll('input, select');
+
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].value.trim() === '') {
+            event.preventDefault(); // Detiene el envío del formulario
+            var fieldName = inputs[i].getAttribute('placeholder') || inputs[i].getAttribute('id');
+            alert('Debes completar el campo: ' + fieldName);
+            return;
+        }
+    }
+
+    // Verificar si el checkbox de términos y condiciones está marcado
+    var termsCheckbox = document.getElementById('terms-checkbox');
+    if (!termsCheckbox.checked) {
+        event.preventDefault(); // Detiene el envío del formulario
+        alert('Debes aceptar los términos y condiciones para continuar.');
+        return;
+    }
+});

b7f503da9347e57b2b3ed70ed7d0e67536ed7952
Azoka23 <marcela68.ma@gmail.com>
2024-05-04 18:05:41 -0300

    cambios

--- Static/js/validacionCampos.js
+++ Static/js/validacionCampos.js
@@ -1,22 +1,54 @@
+// document.getElementById('register-form').addEventListener('submit', function(event) {
+//     var form = event.target;
+//     var inputs = form.querySelectorAll('input, select');
+
+//     for (var i = 0; i < inputs.length; i++) {
+//         if (inputs[i].value.trim() === '') {
+//             event.preventDefault(); // Detiene el envío del formulario
+//             var fieldName = inputs[i].getAttribute('placeholder') || inputs[i].getAttribute('id');
+//             alert('Debes completar el campo: ' + fieldName);
+//             return ;
+//         }
+//     }
+
+    // Verificar si el checkbox de términos y condiciones está marcado
+//    
+
 document.getElementById('register-form').addEventListener('submit', function(event) {
+    // Evitar que el formulario se envíe automáticamente
+    event.preventDefault();
+
     var form = event.target;
     var inputs = form.querySelectorAll('input, select');
 
+    // Limpiar todos los mensajes de error al intentar enviar el formulario
+    var errorFields = form.querySelectorAll('.error-message');
+    errorFields.forEach(function(errorField) {
+        errorField.textContent = ""; // Limpiar el contenido del mensaje de error
+    });
+
+    var errorFound = false;
+
     for (var i = 0; i < inputs.length; i++) {
-        if (inputs[i].value.trim() === '') {
-            event.preventDefault(); // Detiene el envío del formulario
-            var fieldName = inputs[i].getAttribute('placeholder') || inputs[i].getAttribute('id');
-            alert('Debes completar el campo: ' + fieldName);
-            return;
+        var input = inputs[i];
+        if (input.value.trim() === '') {
+            var fieldName = input.getAttribute('placeholder') || input.getAttribute('id');
+            var errorField = document.getElementById('error-' + input.getAttribute('id'));
+            errorField.textContent = 'Debes completar el campo: ' + fieldName;
+            errorFound = true; 
         }
     }
 
     // Verificar si el checkbox de términos y condiciones está marcado
     var termsCheckbox = document.getElementById('terms-checkbox');
     if (!termsCheckbox.checked) {
-        event.preventDefault(); // Detiene el envío del formulario
-        alert('Debes aceptar los términos y condiciones para continuar.');
-        return;
+        var errorField = document.getElementById('error-message');
+        errorField.textContent = 'Debes aceptar los términos y condiciones para continuar.';
+        errorFound = true; 
     }
     
+    if (!errorFound) {
+        
+        form.submit();
+    }
 });
